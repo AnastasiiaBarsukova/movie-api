a69c6c04f5cc4dc53fb35d7a932c52941add4098
Anastasiia Barsukova <nastya2k19@gmail.com>
2026-09-14 20:02:43 +0200

    frontend deleted

--- README.md
+++ README.md
@@ -6,9 +6,6 @@ Als Grundlage für das Projekt wurde die bereits bestehende Beispieldatenbank Pa
 
 Das Projekt wurde erstellt, um die Entwicklung von REST-Services mit Spring Boot, die Implementierung von API-Controllern sowie die Dokumentation von Endpunkten mit OpenAPI und Swagger UI praktisch zu üben.
 
-> **Projektstatus:** In Entwicklung.  
-> Die Funktionen für Schauspieler und Kategorien sind bereits verfügbar. Die Funktionalität für Filme wird derzeit noch erweitert.
-
 ## Technology stack
 
 - Java
--- src/main/java/com/example/filmsAPI/film/FilmRepository.java
+++ src/main/java/com/example/filmsAPI/film/FilmRepository.java
@@ -1,16 +1,23 @@
 package com.example.filmsAPI.film;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import org.jooq.Condition;
 import org.jooq.DSLContext;
+import org.jooq.Field;
+import static org.jooq.Records.mapping;
 import static org.jooq.codegen.maven.example.tables.Actor.ACTOR;
 import static org.jooq.codegen.maven.example.tables.Category.CATEGORY;
 import static org.jooq.codegen.maven.example.tables.Film.FILM;
 import static org.jooq.codegen.maven.example.tables.FilmActor.FILM_ACTOR;
 import static org.jooq.codegen.maven.example.tables.FilmCategory.FILM_CATEGORY;
 import org.jooq.impl.DSL;
+import static org.jooq.impl.DSL.multiset;
+import static org.jooq.impl.DSL.select;
+import static org.jooq.impl.DSL.selectOne;
 import org.springframework.stereotype.Repository;
 
 import com.example.filmsAPI.actor.ActorInfo;
@@ -24,135 +31,202 @@ public class FilmRepository {
         this.dsl = dsl;
     }
 
-    private List<CategoryInfo> joinFilmCategory(Condition condition){
-        return dsl
-                .select(
-                        CATEGORY.CATEGORY_ID,
-                        CATEGORY.NAME
+    private Field<List<CategoryInfo>> joinFilmCategory(){
+        return multiset(
+                        select(
+                                CATEGORY.CATEGORY_ID,
+                                CATEGORY.NAME
+                        )
+                        .from(FILM_CATEGORY)
+                        .join(CATEGORY)
+                        .on(CATEGORY.CATEGORY_ID.eq(
+                                FILM_CATEGORY.CATEGORY_ID
+                        ))
+                        .where(FILM_CATEGORY.FILM_ID.eq(
+                                FILM.FILM_ID
+                        ))
+                        .orderBy(CATEGORY.NAME.asc())
                 )
-                .from(FILM_CATEGORY)
-                .join(CATEGORY)
-                .on(CATEGORY.CATEGORY_ID.eq(
-                        FILM_CATEGORY.CATEGORY_ID
-                ))
-                .where(condition)
-                .fetch(record -> new CategoryInfo(
-                        record.get(CATEGORY.CATEGORY_ID),
-                        record.get(CATEGORY.NAME)
-                ));
+                .convertFrom(result ->
+                        result.map(mapping(CategoryInfo::new))
+                )
+                .as("categories");
     }
 
-    private List<ActorInfo> joinFilmActor(Condition condition){
-        return dsl
-                .select(
-                        ACTOR.ACTOR_ID,
-                        ACTOR.FIRST_NAME,
-                        ACTOR.LAST_NAME
+    private Field<List<ActorInfo>> joinFilmActor(){
+        return multiset( 
+                        select(
+                                ACTOR.ACTOR_ID,
+                                ACTOR.FIRST_NAME,
+                                ACTOR.LAST_NAME
+                        )
+                        .from(FILM_ACTOR)
+                        .join(ACTOR)
+                        .on(ACTOR.ACTOR_ID.eq(
+                                FILM_ACTOR.ACTOR_ID
+                        ))
+                        .where(FILM_ACTOR.FILM_ID.eq(
+                                FILM.FILM_ID
+                        ))
+                        .orderBy(
+                                ACTOR.LAST_NAME.asc(),
+                                ACTOR.FIRST_NAME.asc()
+                        )
                 )
-                .from(FILM_ACTOR)
-                .join(ACTOR)
-                .on(ACTOR.ACTOR_ID.eq(
-                        FILM_ACTOR.ACTOR_ID
-                ))
-                .where(condition)
-                .fetch(record -> new ActorInfo(
-                        record.get(ACTOR.ACTOR_ID),
-                        record.get(ACTOR.FIRST_NAME),
-                        record.get(ACTOR.LAST_NAME)
-                ));
-    }
+                .convertFrom(result ->
+                        result.map(mapping(ActorInfo::new))
+                )
+                .as("actors");
+        }
 
 
     public Optional<FilmInfo> getFilmInfoById(int id) {
-        var filmOptional = dsl
+        Field<List<CategoryInfo>> categories = joinFilmCategory();
+        Field<List<ActorInfo>> actors = joinFilmActor();
+
+        return dsl
                 .select(
                         FILM.FILM_ID,
                         FILM.TITLE,
                         FILM.LENGTH,
-                        FILM.RELEASE_YEAR,
+                        FILM.RELEASE_YEAR.cast(Integer.class),
+                        categories,
+                        actors,
                         FILM.DESCRIPTION
                 )
                 .from(FILM)
                 .where(FILM.FILM_ID.eq(id))
-                .fetchOptional();
-
-        if (filmOptional.isEmpty()) {
-            return Optional.empty();
+                .fetchOptional(mapping(FilmInfo::new));
         }
-
-        List<CategoryInfo> categories = joinFilmCategory(FILM_CATEGORY.FILM_ID.eq(id));
-
-        List<ActorInfo> actors = joinFilmActor(FILM_ACTOR.FILM_ID.eq(id));
-
-        var film = filmOptional.get();
-
-        return Optional.of(new FilmInfo(
-                film.get(FILM.FILM_ID),
-                film.get(FILM.TITLE),
-                film.get(FILM.LENGTH),
-                film.get(FILM.RELEASE_YEAR),
-                categories,
-                actors,
-                film.get(FILM.DESCRIPTION)
-        ));
-    }
 
     public List<FilmInfo> getFilmInfoBy(FilmFilter filter){
         Condition condition = DSL.noCondition();
 
         if (filter.title() != null && !filter.title().isBlank()) {
             condition = condition.and(
-                    FILM.TITLE.containsIgnoreCase(filter.title().trim())
+                FILM.TITLE.containsIgnoreCase(filter.title().trim())
             );
         }
 
         if (filter.minLength() != null) {
             condition = condition.and(
-                    FILM.LENGTH.ge(filter.minLength())
+                FILM.LENGTH.ge(filter.minLength())
             );
         }
 
         if (filter.maxLength() != null) {
             condition = condition.and(
-                    FILM.LENGTH.le(filter.maxLength())
+                FILM.LENGTH.le(filter.maxLength())
             );
         }
 
         if (filter.releaseYear() != null) {
             condition = condition.and(
-                    FILM.RELEASE_YEAR.eq(filter.releaseYear())
+                FILM.RELEASE_YEAR.eq(filter.releaseYear())
             );
         }
 
         if (filter.category() != null && !filter.category().isBlank()) {
-            condition = condition.and(
-                    CATEGORY.NAME.equalIgnoreCase(filter.category())
-            );
+                condition = condition.andExists(
+                        selectOne()
+                                .from(FILM_CATEGORY)
+                                .join(CATEGORY)
+                                .on(CATEGORY.CATEGORY_ID.eq(
+                                        FILM_CATEGORY.CATEGORY_ID
+                                ))
+                                .where(FILM_CATEGORY.FILM_ID.eq(
+                                        FILM.FILM_ID
+                                ))
+                                .and(CATEGORY.NAME.equalIgnoreCase(filter.category()))
+                );
         }
 
-        List<CategoryInfo> categories = joinFilmCategory(condition);
-        List<ActorInfo> actors = joinFilmActor(condition);
+        Field<List<CategoryInfo>> categories = joinFilmCategory();
+        Field<List<ActorInfo>> actors =joinFilmActor();
+                        
 
         return dsl
                 .select(
-                        FILM.FILM_ID,
-                        FILM.TITLE,
-                        FILM.LENGTH,
-                        FILM.RELEASE_YEAR,
-                        CATEGORY.NAME,
-                        FILM.DESCRIPTION
-                )
-                .from(FILM)
-                .where(condition)
-                .orderBy(FILM.TITLE.asc())
-                .fetch(record -> new FilmInfo(
-                        record.get(FILM.FILM_ID),
-                        record.get(FILM.TITLE),
-                        record.get(FILM.LENGTH),
-                        record.get(FILM.RELEASE_YEAR),
-                        categories, 
-                        actors,
-                        record.get(FILM.DESCRIPTION)
-                ));
-    }
+                    FILM.FILM_ID,
+                    FILM.TITLE,
+                    FILM.LENGTH,
+                    FILM.RELEASE_YEAR.cast(Integer.class),     
+                    categories,
+                    actors,
+                    FILM.DESCRIPTION
+            )
+            .from(FILM)
+            .where(condition)
+            .orderBy(FILM.TITLE.asc())
+            .fetch(mapping(FilmInfo::new));
+        }
+
+        public Integer insertFilm(CreateFilmRequest film) {
+                return dsl
+                        .insertInto(FILM)
+                        .set(FILM.TITLE, film.title().trim())
+                        .set(FILM.LENGTH, film.length())
+                        .set(FILM.RELEASE_YEAR, film.releaseYear())
+                        .set(FILM.DESCRIPTION, film.description())
+                        .returningResult(FILM.FILM_ID)
+                        .fetchOptional(FILM.FILM_ID)
+                        .orElseThrow(() -> new IllegalStateException());
+        }
+
+        public void insertFilmCategories(Integer filmId, Collection<Integer> categoryIds) {
+                if (categoryIds == null || categoryIds.isEmpty()) {
+                        return;
+                }
+
+                var records = categoryIds.stream()
+                        .filter(Objects::nonNull)
+                        .distinct()
+                        .map(categoryId -> {
+                        var record = dsl.newRecord(FILM_CATEGORY);
+
+                        record.set(
+                                FILM_CATEGORY.FILM_ID,
+                                filmId
+                        );
+
+                        record.set(
+                                FILM_CATEGORY.CATEGORY_ID,
+                                categoryId
+                        );
+
+                        return record;
+                        })
+                        .toList();
+
+                if (!records.isEmpty()) {
+                        dsl.batchInsert(records).execute();
+                }
+        }
+
+        public void insertFilmActors(Integer filmId, Collection<Integer> actorIds) {
+                if (actorIds == null || actorIds.isEmpty()) {
+                        return;
+                }
+
+                var records = actorIds.stream()
+                        .filter(Objects::nonNull)
+                        .distinct()
+                        .map(actorId -> {
+                                var record = dsl.newRecord(FILM_ACTOR);
+                                record.set(
+                                        FILM_ACTOR.FILM_ID,
+                                        filmId
+                                );
+                                record.set(
+                                        FILM_ACTOR.ACTOR_ID,
+                                        actorId
+                                );
+                                return record;
+                        })
+                        .toList();
+
+                if (!records.isEmpty()) {
+                        dsl.batchInsert(records).execute();
+                }
+        }
 }
